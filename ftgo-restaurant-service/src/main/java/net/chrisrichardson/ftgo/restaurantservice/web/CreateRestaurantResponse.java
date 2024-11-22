aa51cffc3d4530e2a52a09caacf6f8aca8544be1
j0y0nt <115058255+j0y0nt@users.noreply.github.com>
2024-11-22 17:55:12 +0530

    sb ms apps

---
+++ ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/web/CreateRestaurantResponse.java
@@ -0,0 +1,5 @@
+package net.chrisrichardson.ftgo.restaurantservice.web;
+
+public record CreateRestaurantResponse(long id) {
+
+}
---
+++ ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/web/GetRestaurantResponse.java
@@ -0,0 +1,5 @@
+package net.chrisrichardson.ftgo.restaurantservice.web;
+
+public record GetRestaurantResponse(Long id, String name) {
+
+}
